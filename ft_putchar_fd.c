a9a339ea839ed9e59fc48dbfabb71dfa3e83b73f
Abdellah Archtouy <aarchtou@e1r3p3.1337.ma>
2022-10-19 14:17:47 +0100

    commit

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,7 +6,7 @@
 /*   By: aarchtou <aarchtou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 11:07:08 by aarchtou          #+#    #+#             */
-/*   Updated: 2022/10/19 13:42:25 by aarchtou         ###   ########.fr       */
+/*   Updated: 2022/10/19 14:17:40 by aarchtou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ void	ft_putchar_fd(char c, int fd)
 	write(fd, &c, 1);
 }
 
-int	main(void)
-{
-	int	f;
+// int	main(void)
+// {
+// 	int	f;
 
-	// f = creat("foo.txt", 0644);
-	f = open("text.txt",O_WRONLY);
-	ft_putchar_fd('a', f);
-}
+// 	// f = creat("foo.txt", 0644);
+// 	f = open("text.txt",O_WRONLY);
+// 	ft_putchar_fd('a', f);
+// }
